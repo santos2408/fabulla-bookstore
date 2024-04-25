40f1ed2732f81136e640cc74a5d6a46068a6988d
Roger Santos <roger.santos36@gmail.com>
2024-04-25 19:59:53 -0300

    feat: criando uma rota de autenticação

---
+++ frontend/src/pages/AuthPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <base-layout> Authentication Page! </base-layout>
+</template>
+
+<script setup>
+import BaseLayout from "@/layouts/BaseLayout.vue";
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const HomePage = () => import("@/pages/HomePage.vue");
+const AuthPage = () => import("@/pages/AuthPage.vue");
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomePage,
+  },
+  {
+    path: "/auth",
+    name: "auth",
+    component: AuthPage,
   },
   {
     path: "/:pathMatch(.*)*",
