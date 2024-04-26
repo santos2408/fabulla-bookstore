93c59c31a1f78456077bf5482eda6cba6ebe8e2f
Roger Santos <roger.santos36@gmail.com>
2024-04-25 21:24:22 -0300

    fix: formatando e corrigindo o 'type' da prop 'isActive

--- frontend/src/modules/landing-page/components/Navigation/Widget/DropdownProfile/DropdownProfile.vue
+++ frontend/src/modules/landing-page/components/Navigation/Widget/DropdownProfile/DropdownProfile.vue
@@ -2,16 +2,16 @@
   <div
     v-show="props.isActive"
     id="dropdown"
-    class="z-50 hidden rounded-md border border-brand-neutral-100 bg-white text-sm shadow-md lg:block"
+    class="border-brand-neutral-100 z-50 hidden rounded-md border bg-white text-sm shadow-md lg:block"
   >
     <ul class="flex flex-col border-b">
-      <li class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800">
+      <li class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50">
         <router-link :to="{ name: 'home' }" class="gap2 flex items-center gap-2">
           <LockKeyhole size="18" class="text-brand-neutral-800" />
           Alterar senha
         </router-link>
       </li>
-      <li class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800">
+      <li class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50">
         <router-link :to="{ name: 'home' }" class="flex items-center gap-2">
           <BookmarkCheck size="18" class="text-brand-neutral-800" />
           Configurar buscas salvas
@@ -20,13 +20,13 @@
     </ul>
 
     <ul class="flex flex-col border-b">
-      <li class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800">
+      <li class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50">
         <router-link :to="{ name: 'home' }" class="flex items-center gap-2">
           <Ban size="18" class="text-brand-neutral-800" />
           Cancelar assinatura
         </router-link>
       </li>
-      <li class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800">
+      <li class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50">
         <router-link :to="{ name: 'home' }" class="flex items-center gap-2">
           <ContactRound size="18" class="text-brand-neutral-800" />
           Dados pessoais e de contato
@@ -35,14 +35,14 @@
     </ul>
 
     <ul class="flex flex-col">
-      <li class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800">
+      <li class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50">
         <router-link :to="{ name: 'home' }" class="flex items-center gap-2">
           <Library size="18" class="text-brand-neutral-800" />
           Cadastro e assinatura
         </router-link>
       </li>
       <li
-        class="hover:bg-brand-primary-50 cursor-pointer px-4 py-3 text-brand-neutral-800"
+        class="text-brand-neutral-800 cursor-pointer px-4 py-3 hover:bg-brand-primary-50"
         @click="() => emit('logout-user')"
       >
         <router-link :to="{ name: 'home' }" class="flex items-center gap-2">
@@ -62,7 +62,7 @@ const emit = defineEmits(["close-dropdown", "logout-user"]);
 
 const props = defineProps({
   isActive: {
-    type: String,
+    type: Boolean,
     required: true,
   },
 });
